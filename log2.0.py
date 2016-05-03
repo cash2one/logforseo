7a0efffefdc6623e6e70055c57cff291026d359a
PC_souler <291376690@qq.com>
2016-05-03 18:43:59 +0800

    修改

--- log2.0.py
+++ log2.0.py
@@ -9,7 +9,7 @@ import re
 def spider_log(day):
     #返回规定时间的所有蜘蛛的日志（包括百度和谷歌360搜狗）
     spider = []
-    for line in open('text.log'):
+    for line in open('text.log'):  #修改日志文件位置
         line = line.strip()
         if re.findall(r'{day}/May/2016'.format(day=day),line):
             if re.findall(r'Baiduspider',line) or re.findall(r'Googlebot',line) or re.findall(r'360Spider',line) or re.findall(r'Sogou',line):
@@ -28,11 +28,55 @@ def one_log(spiders,search):
 
 
 
+def bad_log(spiders):
+    #获得404蜘蛛日志
+    bad_spider = []
+    for spider in spiders:
+        if re.findall(r'404',spider):
+            bad_spider.append(spider)
+    return bad_spider
+
+def bad_log(spiders):
+    #获得正常的蜘蛛日志
+    nice_spider = []
+    for spider in spiders:
+        if re.findall(r'200',spider) and not re.findall(r'.css',spider) and not re.findall(r'.jss',spider):
+            nice_spider.append(spider)
+    return nice_spider
+
+def other_log(spiders):
+    #获得其他蜘蛛日志
+    other_spider = []
+    for spider in spiders:
+        if not re.findall(r'200',spider) and not re.findall(r'400',spider):
+            other_spider.append(spider)
+    return other_spider
 
 
 
+def download_log(spiders):
+    #通过正则表达式把日志分解成需要的模块
+    re_download = '\s(\d+)\s"'
+    download = 0
+    for spider in spiders:
+        print re.findall(r'\s(\d+)\s"',spider)
+        if  re.findall(re_download,spider):
+             download = int(re.findall(re_download,spider)[0]) + download
+    return download
 
 
+def url_log(spiders):
+    #返回一个所有url的词典，没有经过排序
+    url = {}
+    for spider in spiders:
+        if re.findall(r'\s/(.*?)\sHTTP/1.1',spider):
+            lianjie = re.findall(r'\s/(.*?)\sHTTP/1.1',spider)[0]
+            if lianjie not in url:
+                url[lianjie] = 1
+            else:
+                url[lianjie] = url[lianjie] + 1
+    return url
 
 
-print one_log(spider_log(3),'Baiduspider')
+# print spider_log(3)[1]
+print url_log(spider_log(3))
